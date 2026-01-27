6d6f2ca65b6b94b9310b942d4212ce2e9ee6c385
Gage Henderson <gage@quangoinc.com>
2026-01-27 13:26:55 -0800

    Added some telescope ignore patterns.

--- config/nvim/lua/plugin-configs/telescope-config.lua
+++ config/nvim/lua/plugin-configs/telescope-config.lua
@@ -6,11 +6,22 @@ return function()
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+            file_ignore_patterns = {
+                'node_modules/',
+                '%.git/',
+                'dist/',
+                'build/',
+                '%.next/',
+                '__pycache__/',
+                '%.venv/',
+                'venv/',
+                '%.cache/',
+                'target/',
+                'vendor/',
+                '%.DS_Store',
+            },
+        },
         -- pickers = {}
         extensions = {
             ['ui-select'] = {
